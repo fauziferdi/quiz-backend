fe1ef3965751dc186530e8624687106e35a14fd9
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2026-01-08 09:27:28 +0700

    feat : api login users

--- app.js
+++ app.js
@@ -9,32 +9,57 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// PERBAIKAN DI SINI:
-// 1. Tambahkan '/api' agar sesuai dengan URL di Postman
+//TAMBAH DATA USER
 app.post("/api/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    
+
     const [result] = await db.query(
       "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)",
       [username, email, password]
     );
 
     res.status(201).json({
-        message: "Register berhasil",
-        data: {
-            id: result.insertId,
-            username: username,
-            email: email
-        }
+      message: "Register berhasil",
+      data: {
+        id: result.insertId,
+        username: username,
+        email: email,
+      },
     });
-
   } catch (error) {
-    console.error(error); // Tampilkan error di terminal VS Code agar mudah dicek
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//LOGIN USER
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const [users] = await db.query(
+      "SELECT * FROM users WHERE email = ? AND password_hash = ?",
+      [email, password]
+    );
+    if (users.length > 0) {
+      const user = users[0];
+      res.status(200).json({
+        message: "Login berhasil",
+        data: {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+        },
+      });
+    } else {
+      res.status(401).json({ error: "Email atau password salah" });
+    }
+  } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
+  console.log(`Server berjalan di http://localhost:${PORT}`);
 });
