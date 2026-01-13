5d1d6cc02ce3fd2e1c31116b551770c03b362464
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2026-01-13 23:02:41 +0700

    feat : configuration aiven mysql

--- db.js
+++ db.js
@@ -6,6 +6,10 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 26462, 
+    ssl: {
+        rejectUnauthorized: false 
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
